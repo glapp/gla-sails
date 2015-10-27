be223f0a55d7f943208b2853b2213892baf67e13
Fabio <islerfab@gmail.com>
2015-10-27 17:01:52 +0100

    made skeleton for uploading zip file

--- api/controllers/ComponentController.js
+++ api/controllers/ComponentController.js
@@ -5,7 +5,27 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var async = require('async');
+
 module.exports = {
-	
+	uploadZipFile: function(req, res) {
+    req.file('components').upload({
+      // don't allow the total upload size to exceed ~10MB
+      // maxBytes: 10000000
+    },function whenDone(err, uploadedFiles) {
+      if (err) {
+        return res.negotiate(err);
+      }
+
+      // If no files were uploaded, respond with an error.
+      if (uploadedFiles.length === 0){
+        return res.badRequest('No file was uploaded');
+      }
+
+      async.each(uploadedFiles, function(file, save) {
+
+      })
+    });
+  }
 };
 
